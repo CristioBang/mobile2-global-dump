1df137e8a30da85db353b644156bbd920c01c80e
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2023-08-13 09:28:46 -0800

    eu4
    eu4

---
+++ Mobile2-Dump.cpp
@@ -0,0 +1,22 @@
+
+|{Asm}|
+HP : metin2f-Win64-Shipping.exe+94126D - 89 81 5C080000
+XP : metin2f-Win64-Shipping.exe+933268 - 48 89 82 98080000 
+SP : metin2f-Win64-Shipping.exe+941281 - 44 89 81 64080000 
+Attack Speed : metin2f-Win64-Shipping.exe+9420A5 - 89 81 A8080000
+
+|{Pointer}|
+HP : "metin2f-Win64-Shipping.exe"+03C03DA0 > 0x2F8 > 0x85C
+XP : "metin2f-Win64-Shipping.exe"+03C03DA0 > 0x2F8 > 0x898
+SP : "metin2f-Win64-Shipping.exe"+03C03DA0 > 0x2F8 > 0x864
+Attack Speed : "metin2f-Win64-Shipping.exe"+03C03DA0 > 0x2F8 > 0x8A8
+Movement Speed : "metin2f-Win64-Shipping.exe"+03C03DA0 > 0x2F8 > 0x86C
+
+|{LocalPlayer}|
+Module : "metin2f-Win64-Shipping.exe"
+Pointer : "0x2F8"
+HP : "0x85C"
+XP : "0x898"
+SP : "0x864"
+Attack Speed : "0x8A8"
+Movement Speed : "0x86C"
